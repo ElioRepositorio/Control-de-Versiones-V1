4f023e719895a08c5cc1504526ff8fb00db15efb
ManTequila <ManTequila@DESKTOP-NMA3TGF>
2023-03-01 12:15:11 +0100

    Version 2.2 valor=4

--- src/main/java/com/mycompany/elioal/git/ElioALGIT.java
+++ src/main/java/com/mycompany/elioal/git/ElioALGIT.java
@@ -14,7 +14,7 @@ public class ElioALGIT {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         
-        int valor= 3;
+        int valor= 4;
         
     }
 }
